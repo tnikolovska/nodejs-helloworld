fce97f14c187e3cd897894c12de5efd54628113d
tnikolovska <52244950+tnikolovska@users.noreply.github.com>
2022-05-05 17:55:56 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-curl --request POST --url http://0291-89-205-126-202.ngrok.io/ --header 'content-type: application/json' --data '{"msg": "Hello World!!!"}'
-curl http://0291-89-205-126-202.ngrok.io/
+var expect  = require('chai').expect;
+var request = require('request');
+
+it('Main page content', function(done) {
+    request('http://0291-89-205-126-202.ngrok.io/' , function(error, response, body) {
+        expect(body).to.equal('Hello World!!!');
+        done();
+    });
+});
