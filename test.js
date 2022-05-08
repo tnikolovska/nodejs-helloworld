f3a76abe6d38de3541e60689b670412c483ab269
tnikolovska <52244950+tnikolovska@users.noreply.github.com>
2022-05-08 12:40:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://0291-89-205-126-202.ngrok.io/' , function(error, response, body) {
+    request('http://63ba-89-205-126-245.ngrok.io/' , function(error, response, body) {
         expect(body).to.equal('Hello World!!!');
         done();
     });
